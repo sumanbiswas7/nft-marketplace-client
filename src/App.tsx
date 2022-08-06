ba35afaf5c09a813674a406b209be3df19e6fc51
suman <ronaldosuman97@gmail.com>
2022-08-06 16:00:01 +0530

    nft-item add

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { MetaMaskInpageProvider } from "@metamask/providers";
-import { ethers } from "ethers";
-import { useState } from "react";
+import { ethers, Contract } from "ethers";
+import { useEffect, useState } from "react";
 import marketplaceAddress from "./contractsData/Marketplace-address.json";
 import marketplaceAbi from "./contractsData/Marketplace.json";
 import nftAddress from "./contractsData/NFT-address.json";
@@ -11,17 +11,25 @@ import { Route, Routes } from "react-router-dom";
 import Purchase from "./pages/Purchase";
 import Listed from "./pages/Listed";
 import Create from "./pages/Create";
+import { Item } from "./types/contracts";
+import Home from "./pages/Home";
 declare global {
   interface Window {
     ethereum?: MetaMaskInpageProvider | any;
   }
 }
+interface Props {
+  marketplace: ethers.Contract | null;
+  nft: ethers.Contract | null;
+}
 
 function App() {
   const [account, setAccount] = useState(null);
-  const [nftContract, setNftContract] = useState<ethers.Contract | null>(null);
+  const [nftContract, setNftContract] = useState<Contract | null>(null);
+  const [items, setItems] = useState<Item[]>();
+  const [loading, setLoading] = useState(false);
   const [marketplaceContract, setMarketplaceContract] =
-    useState<ethers.Contract | null>(null);
+    useState<Contract | null>(null);
 
   async function web3Handler() {
     const accounts = await window.ethereum?.request({
@@ -42,19 +50,62 @@ function App() {
 
     setMarketplaceContract(marketplace);
     setNftContract(nft);
-    console.log(nft);
   }
+  async function loadMarketplaceItems(
+    marketplace: ethers.Contract,
+    nft: ethers.Contract
+  ) {
+    const itemCount = await marketplace.itemCount();
+    console.log("total items - ", parseInt(itemCount));
+    const items: Item[] = [];
+    for (let i = 1; i <= itemCount; i++) {
+      const item = await marketplace.s_items(i);
+      if (!item.sold) {
+        const uri = await nft.tokenURI(item.tokenId);
+        const res = await fetch(uri);
+        const metadata = await res.json();
+        // console.log(metadata);
+        const totalPrice = await marketplace.getTotalPrice(item.itemId);
+        items.push({
+          totalPrice,
+          itemId: item.itemId,
+          seller: item.seller,
+          name: metadata.name,
+          description: metadata.description,
+          image: metadata.image,
+        });
+      }
+    }
+    console.log(items);
+    setItems(items);
+    setLoading(false);
+  }
+  async function purchaseItem(item: Item) {
+    await (
+      await marketplaceContract!.purchaseItem(item.itemId, {
+        value: item.totalPrice,
+      })
+    ).wait();
+    loadMarketplaceItems(marketplaceContract!, nftContract!);
+  }
+
+  useEffect(() => {
+    if (account) {
+      loadMarketplaceItems(marketplaceContract!, nftContract!);
+    }
+  }, [account]);
 
   return (
     <div className="App">
       <NavBar web3Handler={web3Handler} account={account} />
       <Routes>
+        <Route path="/" element={<Home items={items!} />} />
         <Route path="/purchase" element={<Purchase />} />
         <Route path="/listed" element={<Listed />} />
         <Route
           path="/create"
           element={
-            <Create marketplace={marketplaceContract} nft={nftContract} />
+            <Create nft={nftContract} marketplace={marketplaceContract} />
           }
         />
       </Routes>
--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,14 +1,34 @@
-import { ethers } from "ethers";
-import React from "react";
+import { Contract, ethers } from "ethers";
+import React, { useEffect, useState } from "react";
 interface Props {
-  marketplace: ethers.Contract | null;
-  nft: ethers.Contract | null;
+  nft: Contract | null;
+  marketplace: Contract | null;
 }
 
 function Create({ marketplace, nft }: Props): JSX.Element {
+  async function mintAndListNft() {
+    const tokenURI =
+      "https://ipfs.infura.io/ipfs/Qmdhb6XwYMDc4i2zazrf5j2iYVQD3kSEmd1meHEWeJFBz5?filename=1.json";
+
+    const mintTx = await nft!.mint(tokenURI);
+    await mintTx.wait();
+
+    const tokenId = await nft!.s_tokenId();
+    const approveTx = await nft!.setApprovalForAll(marketplace!.address, true);
+    await approveTx.wait();
+
+    const listingTx = await marketplace!.makeItem(
+      nft?.address,
+      tokenId,
+      ethers.utils.parseEther("0.99")
+    );
+    listingTx.wait();
+  }
+
   return (
     <>
       <h1>Create Nfts</h1>
+      <button onClick={mintAndListNft}>Create NFT</button>
     </>
   );
 }
